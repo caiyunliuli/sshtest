a551b78c4ac5e88c6d2882dc15c857ffcd0dd314
caiyunliuli <1611303381@qq.com>
2022-11-23 02:03:24 +0800

    test ssh

---
+++ testssh/src/main/java/com/atguigu/Test.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author liuli
+ * @create 2022-11-23 1:59
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test...ssh");
+    }
+}
